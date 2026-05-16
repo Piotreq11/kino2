f73a01127781e110216978e606eb308fd7d2b297
Piotreq11 <p.gorecki1111@wp.pl>
2026-05-16 18:23:01 +0200

    initial commit

---
+++ src/test/java/com/nauka/kino2/Kino2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nauka.kino2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kino2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
